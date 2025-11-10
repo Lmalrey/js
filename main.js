cbe8c5fa9946c4c773df8b9f5873be9df6eb5866
luis <alcalareyesluis@gmail.com>
2025-11-10 11:40:41 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+// let num1=3;
+// let num2=5;
+// let age=23;
+// let bol1=true;
+// let bol2=false;
+// let bol3=true;
+// let pi=Math.PI;
+// let null_var=null;
+
+
+// console.log(num1 * num2);
+
+// console.log(`Hola mundo, tengo ${age} años`);
+
+// let result1= bol1 && bol2;
+// console.log(result1);
+
+// let result2=bol1 || bol2;
+// console.log(result2);
+
+// let result3=bol1 && bol3;
+// console.log(result3);
+
+// console.log(`¿Es ${num1} mayor que ${pi}? => ${num1>pi}`);
+
+// num1 ++;
+
+// console.log(`¿Es ${num1} mayor que ${pi}? => ${num1>pi}`);
+
+// console.log(typeof(num1)== 'number');
+// console.log(typeof(bol1));
+// console.log(typeof null_var);
+
+/**
+ * 1. Declare two variables, a = 10 and b = 3.
+ * Calculate the remainder when a is divided by b and store it
+ * in a variable called remainder.
+ */
+
+// let a=10;
+// let b=3;
+
+// remainder= a % b;
+// console.log(remainder);
+
+/**
+ * 2. Declare a variable age = 25.
+ * Write an expression using a comparison operator to check if age
+ * is greater than or equal to 18. Store the result in a variable calle
+ * isAdult.
+ */
+
+// let age=25;
+// let isAdult = age>= 18;
+
+// console.log(isAdult);
+
+/**
+ * 3. Declare a boolean variable isRaining = false.
+ * Use the logical NOT operator (!) to reverse its value and store
+ * the result in shouldStayInside.
+ */
+
+// let isRaining=false;
+// let shouldStayInside=!isRaining;
+// console.log(shouldStayInside);
+
+/**
+ * 
+ */
